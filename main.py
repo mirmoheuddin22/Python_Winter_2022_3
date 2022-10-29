20bc18e2d8a2fd62f441ccdf686ec5cf4e8579ba
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-10-29 12:54:46 +0200

    Comment to demo gi

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+	# New comment to demo git
+	print('Hello from git')
+	
 
 
 
