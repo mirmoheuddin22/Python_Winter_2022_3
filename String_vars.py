7b4915d470d03edbde013548e4a9e79fbfda9b44
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-10-29 14:37:36 +0200

    String vars

---
+++ String_vars.py
@@ -0,0 +1,20 @@
+s1 = 'sfhkjdfsjnsfk'
+print(s1[2])
+print(s1[2:6])
+print(s1[2:])
+print(s1[:2])
+print(s1[-2:])
+s2 = s1[:2] + s1[-2:]
+print(s2)
+
+print(s1.find('fs'))
+s3a = '23453444;23444433;24553435;245355'
+s3b = "24234355;23554555;55325355;352535"
+s4 = "I can't"
+s5 = 'He said "my name is....."'
+s6 = 'He said "I can\'t"'
+
+print(s3a.split(';'))
+print(s3b.split(';')[1])
+
+
