079b593fee8685f0e48d3cb6d3787993b4e501f8
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-10-29 14:26:22 +0200

    Numeric vars

---
+++ numeric_vars.py
@@ -0,0 +1,43 @@
+a = 45566765455666666566546776554
+b = 45952537589858335384255579359
+print(a)
+print(b)
+f = 42453534435456436663454566465.5
+print(f)
+g = f + 1000000
+print(g)
+h = 0.000000000000003445
+print(h)
+print(type(h))
+print(type(b))
+i = 1.79e308
+j = 1.8e308
+print(i)
+print(j)
+
+print('-----------')
+bb = 0b101
+print(bb)
+print('------------')
+hx = 0x1a
+print(hx)
+print('------------')
+print(2**8)
+# rest of division
+print(19 % 9)
+a = 200
+b = 5
+c = a/b
+print(c)
+print(type(c))
+c = 50.6336
+print(int(c))
+print(round(c))
+print(round(c, 2))
+s1 = '64'
+s2 = '25.5425'
+print(s1.isnumeric())
+# Test if is an int number
+ee = int(s1) + float(s2)
+print(ee)
+
