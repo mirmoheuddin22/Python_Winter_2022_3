a2bfebed44c62f0cabe013680fe2e2fbb208c277
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-10-30 15:33:14 +0100

    Data Visualization

--- Data Visualization.py
+++ Data Visualization.py
@@ -1,19 +1,3 @@
 import pandas as pd
-s1 = pd.Series([1,2,3])
-s2 = pd.Series([4,5,6])
-cols = ["A", "B", "C"]
-df = pd.DataFrame([list(s1), list(s2)], columns = cols) # df = DataFrame
-print(df)
-new_row = [7,8,9]
-df.loc[-1] = new_row
-print(df)
-df.reset_index(drop = True)
-print(df.reset_index(drop = True))
-new_row = [7, 8, 9]
-new_df = pd.DataFrame([new_row], columns = cols)
-print(new_df)
-df = pd.concat([new_df, df], ignore_index = True)
-print(df)
-import numpy as np
-df = pd.DataFrame(np.insert(df.values, 1, new_row, axis = 0),columns =cols)
+df = pd.read_csv(r"D:\Data set\read.csv")
 print(df)
