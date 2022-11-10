2b447ea037e59dbddfc7ac75e994387ed960371d
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-11-10 20:25:01 +0100

    Variables!

---
+++ blocks-variables visibility.py
@@ -0,0 +1,15 @@
+a = 3
+for i in range(4):
+    b = 5
+    print(f'i={i}')
+    print(f'a={a}')
+print('--------')
+print(f'b={b}')
+
+if a > 4:
+    if b > 3:
+        b = 50
+    else:
+        b = 10
+print(f'b={b}')
+
