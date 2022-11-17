d931965a2320134b8cab3643421c0f435de599e9
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-11-17 14:39:44 +0100

    list 2D!

---
+++ lists_2D.py
@@ -0,0 +1,12 @@
+l1 = [[5, 7, 8],
+      [3, 5, 6],
+      [4, 2, 1]]
+print(l1)
+l1_0 = l1[0]
+print(l1_0)
+print(l1_0[2])
+print(l1[0][2])
+
+#TODO sum all rows in l1 as a list of values
+#TODO sum all columns in l1 as a list of values
+#TODO sum all elements of l1
