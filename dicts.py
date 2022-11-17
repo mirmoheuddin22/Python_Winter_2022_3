cf95a05059342c500b1cc3c7c10025b7879bb80f
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-11-17 21:17:48 +0100

    Dictionary!

--- dicts.py
+++ dicts.py
@@ -12,3 +12,36 @@ for k, v in d2.items():
 print('---------')
 for v in d2.values():
      print(v)
+My_team = dict([ ('Colorado', 'Rockies'), ('Boston', 'Red Sox'), ('Minnesota', 'Twins'), ('Milwaukee', 'Brewers'), ('Seattle', 'Mariners')])
+print(My_team)
+My_team = dict( Colorado='Rockies', Boston='Red Sox', Minnesota='Twins', Milwaukee='Brewers', Seattle='Mariners' )
+print(My_team)
+print(type(My_team))
+print(My_team['Minnesota'])
+#Adding an entry to an existing dictionary:
+My_team['Kansas City'] = 'Royals'
+print(My_team)
+#To delete an entry:
+del My_team['Seattle']
+print(My_team)
+d = {0: 'a', 1: 'b', 2: 'c', 3: 'd'}
+print(d[0])
+d = {(1, 1): 'a', (1, 2): 'b', (2, 1): 'c', (2, 2): 'd'}
+print((1, 1))
+d = {0: 'a', 1: 'a', 2: 'a', 3: 'a'}
+print(d[0] == d[1] == d[2])
+print('Milwaukee' in My_team)
+print(len(My_team))
+d = {'a': 10, 'b': 20, 'c': 30}
+print(d.clear())
+d = {'a': 10, 'b': 20, 'c': 30}
+print(d.get('b'))
+d = {'a': 10, 'b': 20, 'c': 30}
+print(list(d.items()))
+print(list(d.items())[1][0])
+print(list(d.items())[1][1])
+print(list(d.keys()))
+print(list(d.values()))
+print(d.pop('b'))
+print(d.pop('z', -1))
+print(d.popitem())
